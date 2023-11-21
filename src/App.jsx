25115d02ba5b24c22f9785d56aca22551d991a1b
Beni-higher <dikitelebeni@gmail.com>
2023-11-21 16:22:54 -0500

    first coommit to initialize the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    
+  return <>
+            <h1 id="title" className="myClasse">Title</h1>
+    <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione voluptatibus repellendus libero labore? Perspiciatis
+       vero laborum repellendus doloremque. Animi beatae eaque voluptatum suscipit explicabo sed odit laboriosam delectus commodi. Odit!</p>
+  </>
+}
+
+export default App
